fce2c68528ac777598a3c4b228e6c8dc569fe199
Coleman McFarland <cmcfarland@paperlesspost.com>
2019-02-24 20:34:14 -0500

    WIP: still fighting the borrow checker

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use std::io;
 use std::io::Read;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::process;
 use std::mem::size_of;
+use std::process::ChildStdout;
+use std::process::ChildStderr;
 
 static USAGE: &str = r#"sled-exec - wrap a command and store the standard streams in a sled database
 
@@ -16,6 +19,9 @@ Options:
     --db PATH       Path to open or create sled database; Default: "sled-exec.db" in current directory
     --compress      Enable sled's log compression
 "#;
+
+// I guess this works for \r\n too.
+const NEWLINE: u8 = 0xA;
 
 fn main() -> Result<(), std::io::Error> {
 
@@ -61,7 +67,7 @@ fn main() -> Result<(), std::io::Error> {
         exit_with_message(1, USAGE);
     }
     let mut iter = subcommand_args.iter();
-    let mut cmd = std::process::Command::new(iter.next().unwrap());
+    let mut cmd = process::Command::new(iter.next().unwrap());
     while let Some(arg) = iter.next() {
         cmd.arg(arg);
     }
@@ -69,53 +75,67 @@ fn main() -> Result<(), std::io::Error> {
     let mut tree = sled::Db::start(config.build()).expect("could not open database");
 
     let mut child = cmd.spawn()?;
-    let mut child_stderr = BufReader::new(child.stderr.expect("child missing stderr"));
-    let mut child_stdout = BufReader::new(child.stdout.expect("child missing stdout"));
+    // TODO we might not have either of these streams, so don't panic
+//    let mut child_stderr = BufReader::new(child.stderr.expect("child missing stderr"));
+//    let mut child_stdout = BufReader::new(child.stdout.expect("child missing stdout"));
+    let mut child_stdout = child.stdout.map_or(None, |stream| Some(BufReader::new(stream)));
+    let mut child_stderr = child.stderr.map_or(None, |stream| Some(BufReader::new(stream)));
+
+
 
     // collect our output into these buffers
-    let mut line_stdout = Vec::new();
-    let mut line_stderr = Vec::new();
+    let mut line_stdout: Vec<u8> = Vec::new();
+    let mut line_stderr: Vec<u8> = Vec::new();
 
     // Keep track of EOF on both streams
     let (mut eof_stdout, mut eof_stderr) = (false, false);
 
-    // I guess this works for \r\n too.
-    const NEWLINE: u8 = 0xA;
+    if let None = child_stdout {
+       eof_stdout = true
+    }
+
 
     loop {
-        if !eof_stdout {
-            match child_stdout.read_until(NEWLINE, &mut line_stdout) {
-                Ok(n) if n == 0 => {
-                    eof_stdout = true;
-                }
-                Ok(n) => {
-                    let next_id = tree.generate_id().unwrap();
-                    tree.set(format!("stdout:{:08}", next_id), line_stdout.clone()).unwrap().unwrap();
-                }
-                Err(e) => {
-                    // There COULD be bytes in our buffer
-                    eof_stdout = true;
-                }
-            };
-            line_stdout.clear();
-        }
 
-        if !eof_stderr {
-            match child_stderr.read_until(NEWLINE, &mut line_stderr) {
-                Ok(n) if n == 0 => {
-                    eof_stderr = true;
-                }
-                Ok(n) => {
-                    let next_id = tree.generate_id().unwrap();
-                    tree.set(format!("stderr:{:08}", next_id), line_stderr.clone()).unwrap().unwrap();
-                }
-                Err(e) => {
-                    // There COULD be bytes in our buffer
-                    eof_stderr = true;
-                }
-            };
-            line_stderr.clear();
-        }
+//                if !eof_stdout {
+//                    eof_stdout = maybe_write_line(&mut stream, &mut tree, &mut line_stdout);
+//                }
+
+
+            if !eof_stdout {
+                match child_stdout.unwrap().read_until(NEWLINE, &mut line_stdout) {
+                    Ok(n) if n == 0 => {
+                        eof_stdout = true;
+                    }
+                    Ok(n) => {
+                        let next_id = tree.generate_id().unwrap();
+                        tree.set(format!("stdout:{:08}", next_id), line_stdout.clone()).unwrap().unwrap();
+                    }
+                    Err(e) => {
+                        // There COULD be bytes in our buffer
+                        eof_stdout = true;
+                    }
+                };
+                line_stdout.clear();
+            }
+
+
+//        if !eof_stderr {
+//            match child_stderr.read_until(NEWLINE, &mut line_stderr) {
+//                Ok(n) if n == 0 => {
+//                    eof_stderr = true;
+//                }
+//                Ok(n) => {
+//                    let next_id = tree.generate_id().unwrap();
+//                    tree.set(format!("stderr:{:08}", next_id), line_stderr.clone()).unwrap().unwrap();
+//                }
+//                Err(e) => {
+//                    // There COULD be bytes in our buffer
+//                    eof_stderr = true;
+//                }
+//            };
+//            line_stderr.clear();
+//        }
 
         if eof_stderr && eof_stdout {
             break;
@@ -123,6 +143,26 @@ fn main() -> Result<(), std::io::Error> {
     }
 
     Ok(())
+}
+
+fn maybe_write_line<T: Read>(stream: &mut BufReader<T>, db: &mut sled::Db, mut line_buf: &mut Vec<u8>) -> bool {
+    let mut eof = false;
+        match stream.read_until(NEWLINE, &mut line_buf) {
+                Ok(n) if n == 0 => {
+                    eof = true;
+                }
+                Ok(n) => {
+                    let next_id = db.generate_id().unwrap();
+                    // TODO: prefix with stdout/stderr
+                    db.set(format!("{}", next_id), line_buf.clone()).unwrap().unwrap();
+                }
+                Err(e) => {
+                    // There COULD be bytes in our buffer
+                    eof = true;
+                }
+            };
+            line_buf.clear();
+    eof
 }
 
 fn exit_with_message(code: i32, msg: &str) {
